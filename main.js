bac741f31a7f91f025b0607f02125d56b40edb17
zarar14 <67764461+zarar14@users.noreply.github.com>
2025-05-25 01:21:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text1",{
+    strings:["Software Developer", "Web Developer","AI Enthusiast"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
